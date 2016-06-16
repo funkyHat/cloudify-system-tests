b4a63fcfdbaf0a2960c875d25fac9d7773ffa4e8
Matt Wheeler <m@funkyhat.org>
2016-06-16 13:27:59 +0100

    Move framework into a dir so setup.py makes sense

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="cloudify_tester",
     version="0.1.0",
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
+    packages=['cloudify_tester'],
     package_data={'cloudify_tester': [
         'schemas/*.yaml',
     ]},
     install_requires=[
         'pytest',
         'PyYAML==3.11',
+        'cloudify>=3.4a5',
     ],
 )
