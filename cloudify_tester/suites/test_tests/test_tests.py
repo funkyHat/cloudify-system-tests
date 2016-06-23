676803e527c329fbb6111707542f5ad06eb8263a
Matt Wheeler <m@funkyhat.org>
2016-06-23 11:55:13 +0100

    Add a test that demonstrates the config fixture a bit

--- cloudify_tester/suites/test_tests/test_tests.py
+++ cloudify_tester/suites/test_tests/test_tests.py
@@ -1,6 +1,15 @@
+import pytest
 
 
 def test_the_tests(get_all_the_tests):
     """Check that all the tests are having a nice time"""
     for test in get_all_the_tests:
         assert 'test' in test.name
+
+
+def test_the_config(config):
+    """Testing the config fixture"""
+    assert config['real'] is True
+
+    with pytest.raises(KeyError):
+        config['non existent config key']
