e491d7e5d4b26028e8b3eb92d4bd008b9eb2de74
Matt Wheeler <m@funkyhat.org>
2016-07-01 14:22:59 +0100

    Really simple PoC for making pytest-bdd print out the steps as it goes

--- bdd_example/conftest.py
+++ bdd_example/conftest.py
@@ -5,3 +5,7 @@ pytest_plugins = (
     'cloudify_tester.framework.cfy',
     'cloudify_tester.framework.git',
     )
+
+
+def pytest_bdd_before_step_call(request, feature, scenario, step, *args):
+    print('{} {}'.format(step.keyword, step.name))
